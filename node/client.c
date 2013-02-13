035abb7a4cd51a5158798dff2cc12e01d9a2a3b8
flasheur11 <flash.11@hotmail.fr>
2013-02-13 23:45:47 +0100

    Client done

--- node/client.c
+++ node/client.c
@@ -44,11 +44,31 @@ void proceed_task(struct mlc_packet_header *header,char *buffer)
 }
 
 
-char* append_buffer(struct string_packet str_p,int length,char* src)
+struct string_packet append_buffer(struct string_packet str_p,int length,char* src)
 {
+
+        if(str_p.length > str_p.size)
+        {
+            str_p.size *= 2;
+            str_p.buffer = realloc(str_p.buffer,str_p.size);
+        }
+
         for(int i=0 ; i<length; i++)
             str_p.buffer[str_p.length + i] = src[i];
-        return str_p.buffer;
+
+        str_p.length += length;
+        return str_p;
+}
+
+struct string_packet drain_buffer(struct string_packet str_p, size_t n)
+{
+    char* new=malloc(str_p.size);
+
+    for(int i=0; i < (str_p.length-n); i++)
+        new[i] = str_p.buffer[i + n];
+    str_p.buffer = new;
+
+    return str_p;
 }
 
 char* strncpy_buffer(char *dest,const char *src,size_t n)
@@ -59,24 +79,27 @@ char* strncpy_buffer(char *dest,const char *src,size_t n)
     return dest;
 }
 
-int check_rec(struct string_packet str_p)
+struct string_packet check_rec(struct string_packet str_p)
 {
     if(str_p.length >= sizeof(struct mlc_packet_header))
     {
-        for(int i=0; i<80;i ++)
-        printf("%x",*(str_p.buffer + i));
-
         struct mlc_packet_header *header;
 
         char *header_str=malloc(sizeof(struct mlc_packet_header));
         header_str = strncpy_buffer(header_str,str_p.buffer,sizeof(struct mlc_packet_header));
         
         header = (struct mlc_packet_header*)header_str;
-        proceed_task(header,"");
-
-        
+        if(str_p.length >= header->size_of)
+        {
+            str_p = drain_buffer(str_p,sizeof(struct mlc_packet_header));
+            char *data=malloc(header->size_of);
+            data = strncpy(data,str_p.buffer,header->size_of);
+            str_p = drain_buffer(str_p,header->size_of);
+            str_p.length -= header->size_of;
+            proceed_task(header,data);
+        }
     }
-    return 1;
+        return str_p;
 }
 
 void rec_buffer(int socket)
@@ -94,19 +117,10 @@ void rec_buffer(int socket)
 
     while((length = recv(socket,buffer,1024,0))>0) 
     {
-        str_p.length += length;
-
-        if(str_p.length > str_p.size)
-        {
-            str_p.size*=2;
-            str_p.buffer=realloc(str_p.buffer,str_p.size);
-        }
-        
-        str_p.buffer = append_buffer(str_p,length,buffer);
-        if(check_rec(str_p))
-        {
+        str_p = append_buffer(str_p,length,buffer);
+        str_p = check_rec(str_p);
+        if(str_p.length <= 0)
             break;
-        }
     }
 
 
