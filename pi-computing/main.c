abae773deca93a28b9f31ad3ed3c0d47fad46388
camilleblondie <dollecamille@gmail.com>
2013-04-16 13:01:55 +0200

    update pi-computing

--- pi-computing/main.c
+++ pi-computing/main.c
@@ -2,17 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 #include "../static-lib/lib/mlc.h"
+#include "pi_server.h"
+
+double somme = 0.0;
 
 char* gen_task(int id)
 {
     char *str = malloc(1024);
-    strcpy(str, "echo Salut");
+    char *plg = generate_plg(id,10000000);
+    strcpy(str, "./application/pi_client ");
+    strcat(str, plg);
+    strcat(str, " 0.0");
+    printf("Generate task %d\n", id);
     return str;
 }
 
 void get_result(int id, char* data)
 {
     printf("--- \n Received result. \n Task : %d \n Data : %s \n --- \n", id, data);
+    double result = strtod(data, NULL);
+    int res = atoi(data);
+    if (res == -1)
+     system("pkill dispatcher");
+    make_sum(result, &somme);
 }
 
 int main(int argc, char* argv[])
